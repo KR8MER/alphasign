4ca27e4c574617ee5f5a3433ea837ba0c4bd9c72
KR8MER <69276965+KR8MER@users.noreply.github.com>
2024-07-19 12:16:32 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+SIGN_IP = "192.168.2.250"
+SIGN_PORT = 10001
+SIGN_ADDRESS = "01"
+NWS_CAP_URL = "https://api.weather.gov/alerts/active?zone=MDZ013"
+POLL_INTERVAL = 60  # Seconds
+VERIFY_DELAY = 2  # Seconds
+MAX_RETRIES = 3
+RETRY_DELAY = 5  # Seconds
+
+# Color codes
+GREEN = "\x1C2"
+YELLOW = "\x1C8"
+RED = "\x1C1"
